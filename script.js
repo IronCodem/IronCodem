04ea2689ee092e0be07330b5ae104a821ce4bc5a
Yashas Shah <79524299+YashasTheDev@users.noreply.github.com>
2021-04-20 14:48:32 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// MoodyDevTeam - Headmaster Badge
+/******************************
+* Applied To | Yashas Shah    *
+* Role       | Software Dev   *
+* --------------------------- *
+* Replit     | @YashasShah    *
+* GitHub     | @YashasTheDev  *
+* --------------------------- *
+* Badge Type | Headmaster     *
+******************************/
+
